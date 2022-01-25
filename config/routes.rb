c49e689682d314bc2bf80a35c76687e6df953260
Niklas Coors <coors.niklas@gmail.com>
2022-01-25 19:23:24 +0100

    changed root

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # a user can see all the lists
   # a user can see the details of a given list and its name
   # a user can crea a new list
+
+  root to: 'lists#index'
+
   resources :lists, only: [:index, :new, :show, :create] do
     resources :bookmarks, only: [:index, :new, :create]
   end
